b412185b318f0ea2a17fb30db20440e735665284
Kiratsuji <132310994+Kiratsuji@users.noreply.github.com>
2026-05-23 11:34:56 -0300

    hash router changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ProjectsPage from "./pages/Projects";
@@ -6,13 +6,13 @@ import ContactPage from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<About />} />
         <Route path="/projetos" element={<ProjectsPage />} />
         <Route path="/contato" element={<ContactPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }

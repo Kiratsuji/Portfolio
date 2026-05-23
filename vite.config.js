8de09c32cae951c3dc6ead5569d184d0de0cbd6e
Kiratsuji <132310994+Kiratsuji@users.noreply.github.com>
2026-05-23 11:31:11 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/kiratsuji.github.io/",
+  base: "/Portfolio/",
   plugins: [react(), tailwindcss()],
 });
